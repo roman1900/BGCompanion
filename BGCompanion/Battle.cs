559355ed8caf912e944e927520d2c96f27e13d95
Matt Marschall <mmarschall@redmars.com.au>
2020-09-29 15:35:36 +1000

    Prototype complete for combat phases

--- BGCompanion/Battle.cs
+++ BGCompanion/Battle.cs
@@ -10,6 +10,20 @@ namespace BGCompanion
         public static int winCount;
         public static int loseCount;
         public static int totalBattles;
+        public enum CombatPhase
+        {
+            attack,
+            whenever,
+            reborn,
+            deathrattle
+        }
+        public struct CombatPosition
+        {
+            public int[] AttackQ;
+            public int Attacker;
+            public int Target;
+            public CombatPhase Phase;
+        }
         public static void Simulate(Hand mine, Hand enemy)
         {
             //Rules of Engagement
@@ -39,115 +53,140 @@ namespace BGCompanion
             {
                 winCount++;
             }
-            else if (_mine.slots.Count>_enemy.slots.Count)
+            else if (_mine.slots.Count > _enemy.slots.Count)
             {
-                Combat(new Hand[] { _mine, _enemy }, new int[] { 0, 0 }, 0, -1); 
+                Combat(new Hand[] { _mine, _enemy }, new CombatPosition { AttackQ = new int[] { 0, 0 }, Attacker = 0, Target = -1, Phase = CombatPhase.attack });
             }
-            else if (_enemy.slots.Count>_mine.slots.Count)
+            else if (_enemy.slots.Count > _mine.slots.Count)
             {
-                Combat(new Hand[] { _mine, _enemy }, new int[] { 0, 0 }, 1, -1); 
+                Combat(new Hand[] { _mine, _enemy }, new CombatPosition { AttackQ = new int[] { 0, 0 }, Attacker = 1, Target = -1, Phase = CombatPhase.attack });
             }
             else
             {
-                Combat(new Hand[] { _mine, _enemy }, new int[] { 0, 0 }, 0, -1);
+                Combat(new Hand[] { _mine, _enemy }, new CombatPosition { AttackQ = new int[] { 0, 0 }, Attacker = 0, Target = -1, Phase = CombatPhase.attack });
                 _mine.slots = new List<Card>(mine.slots);
                 _enemy.slots = new List<Card>(enemy.slots);
-                Combat(new Hand[] { _mine, _enemy }, new int[] { 0, 0 }, 1, -1);
+                Combat(new Hand[] { _mine, _enemy }, new CombatPosition { AttackQ = new int[] { 0, 0 }, Attacker = 1, Target = -1, Phase = CombatPhase.attack });
             }
 
 
 
         }
-        private static void Combat(Hand[] Hands,int[] attackQ, int currentAttacker, int target)
+        private static void Combat(Hand[] Hands, CombatPosition combatPosition)//int[] attackQ, int combatPosition.Attacker, int target)
         {
             //TODO(#15): Modify Combat system so a call to Combat can jump to a particular stage of the combat ie. Attack, Reborn, Whenever, DeathRattle etc
-            Hand[] _Hands = new Hand[] { new Hand(), new Hand()};
+            Hand[] _Hands = new Hand[] { new Hand(), new Hand() };
             _Hands[0].slots = Hands[0].slots.ConvertAll<Card>(m => new Card(m));
             _Hands[1].slots = Hands[1].slots.ConvertAll<Card>(m => new Card(m));
             List<Card>[] Taunts = new List<Card>[] { _Hands[0].slots.FindAll(m => m.Taunt), _Hands[1].slots.FindAll(m => m.Taunt) };
-            
-            if (target == -1)
+            if (combatPosition.Phase == CombatPhase.attack && combatPosition.Target == -1)
             {
                 //TODO(#6): Start of Combat effects
                 //TODO(#8): Zapp Slywick always hits lowest attack (bypass taunt)
-                if (Taunts[currentAttacker ^ 1].Count > 0) //the non attacking hands taunts
+                if (Taunts[combatPosition.Attacker ^ 1].Count > 0) //the non attacking hands taunts
                 {
-                    for (int i = 0; i < Taunts[currentAttacker ^ 1].Count; i++)
+                    for (int i = 0; i < Taunts[combatPosition.Attacker ^ 1].Count; i++)
                     {
-                        target = _Hands[currentAttacker ^ 1].slots.FindIndex(m => m == Taunts[currentAttacker ^ 1][i]);
-                        Combat(_Hands, attackQ, currentAttacker, target);
+                        combatPosition.Target = _Hands[combatPosition.Attacker ^ 1].slots.FindIndex(m => m == Taunts[combatPosition.Attacker ^ 1][i]);
+                        Combat(_Hands, combatPosition);
 
                     }
                 }
                 else
                 {
-                    for (int i = 0; i < _Hands[currentAttacker ^ 1].slots.Count; i++ )
+                    for (int i = 0; i < _Hands[combatPosition.Attacker ^ 1].slots.Count; i++)
                     {
-                        Combat(_Hands, attackQ, currentAttacker, i);
+                        combatPosition.Target = i;
+                        Combat(_Hands, combatPosition);
                     }
                 }
             }
             else
             {
+                // Do the whenever phase if startng an attack
+                int wheneverMode = 0;
+                if (combatPosition.Phase == CombatPhase.attack)
+                {
+                    combatPosition.Phase = CombatPhase.whenever;
+                    wheneverMode = 1;
+                }
+
+                if (combatPosition.Phase == CombatPhase.whenever)
+                {
+                    if (wheneverMode == 1) //First time here this attack
+                    {
+                        List<Card> wheneverAttacks = new List<Card>(_Hands[combatPosition.Attacker].slots.FindAll(m => m.buffs.Exists(b => b.What == Buffs.whenEver && b.Trigger == WheneverTrigger.attacks)));
+                        for (int i =0; i < wheneverAttacks.Count; i++)
+                        {
+                            //Combat(_Hands, combatPosition);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+
                 //do some hitting
                 //TODO(#9): Cleave hits adjacent minions
                 //TODO(#10): Windfury 
                 //TODO(#11): Process Whenever effects
                 //Calculate impact on attacker
-                //Console.WriteLine("{0} in postion {1} attacks {2} in position {3}", _Hands[currentAttacker].slots[attackQ[currentAttacker]].Name, attackQ[currentAttacker], _Hands[currentAttacker ^ 1].slots[target].Name, target);
-                if (_Hands[currentAttacker].slots[attackQ[currentAttacker]].DivineShield)
+                //Console.WriteLine("{0} in postion {1} attacks {2} in position {3}", _Hands[combatPosition.Attacker].slots[attackQ[combatPosition.Attacker]].Name, attackQ[combatPosition.Attacker], _Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].Name, combatPosition.Target);
+                if (_Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].DivineShield)
                 {
-                    _Hands[currentAttacker].slots[attackQ[currentAttacker]].DivineShield = false;
+                    _Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].DivineShield = false;
 
                 }
-                else if (_Hands[currentAttacker ^ 1].slots[target].Poisonous)
+                else if (_Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].Poisonous)
                 {
-                    _Hands[currentAttacker].slots[attackQ[currentAttacker]].Health = -99;
+                    _Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].Health = -99;
                 }
                 else
                 {
-                    _Hands[currentAttacker].slots[attackQ[currentAttacker]].Health -= _Hands[currentAttacker ^ 1].slots[target].Attack;
-                    //Console.WriteLine("{0} in position {1} new health is {2}", _Hands[currentAttacker].slots[attackQ[currentAttacker]].Name, attackQ[currentAttacker], _Hands[currentAttacker].slots[attackQ[currentAttacker]].Health);
+                    _Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].Health -= _Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].Attack;
+                    //Console.WriteLine("{0} in position {1} new health is {2}", _Hands[combatPosition.Attacker].slots[attackQ[combatPosition.Attacker]].Name, attackQ[combatPosition.Attacker], _Hands[combatPosition.Attacker].slots[attackQ[combatPosition.Attacker]].Health);
                 }
                 //Calculate impact on defender
-                if (_Hands[currentAttacker ^ 1].slots[target].DivineShield)
+                if (_Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].DivineShield)
                 {
-                    _Hands[currentAttacker ^ 1].slots[target].DivineShield = false;
+                    _Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].DivineShield = false;
                 }
-                else if (_Hands[currentAttacker].slots[attackQ[currentAttacker]].Poisonous)
+                else if (_Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].Poisonous)
                 {
-                    _Hands[currentAttacker ^ 1].slots[target].Health = -99;
+                    _Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].Health = -99;
                 }
                 else
                 {
-                    _Hands[currentAttacker ^ 1].slots[target].Health -= _Hands[currentAttacker].slots[attackQ[currentAttacker]].Attack;
-                    //Console.WriteLine("{0} in position {1} new health is {2}", _Hands[currentAttacker ^ 1].slots[target].Name,target, _Hands[currentAttacker ^ 1].slots[target].Health);
+                    _Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].Health -= _Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].Attack;
+                    //Console.WriteLine("{0} in position {1} new health is {2}", _Hands[combatPosition.Attacker ^ 1].slots[target].Name,target, _Hands[combatPosition.Attacker ^ 1].slots[target].Health);
                 }
 
 
                 //TODO(#12): Process deathrattles
                 //TODO(#13): Process reborn
-                if (_Hands[currentAttacker ^ 1].slots[target].Health <= 0)
+                if (_Hands[combatPosition.Attacker ^ 1].slots[combatPosition.Target].Health <= 0)
                 {
-                    if (attackQ[currentAttacker ^ 1] > target)
+                    if (combatPosition.AttackQ[combatPosition.Attacker ^ 1] > combatPosition.Target)
                     {
-                        attackQ[currentAttacker ^ 1]--;
+                        combatPosition.AttackQ[combatPosition.Attacker ^ 1]--;
                     }
-                    _Hands[currentAttacker ^ 1].slots.RemoveAt(target);
+                    _Hands[combatPosition.Attacker ^ 1].slots.RemoveAt(combatPosition.Target);
                 }
-                if (_Hands[currentAttacker].slots[attackQ[currentAttacker]].Health <= 0)
+                if (_Hands[combatPosition.Attacker].slots[combatPosition.AttackQ[combatPosition.Attacker]].Health <= 0)
                 {
-                    //Console.WriteLine("removing {0} at postion {1} from hand", _Hands[currentAttacker].slots[attackQ[currentAttacker]].Name, attackQ[currentAttacker]);
-                    _Hands[currentAttacker].slots.RemoveAt(attackQ[currentAttacker]);
+                    //Console.WriteLine("removing {0} at postion {1} from hand", _Hands[combatPosition.Attacker].slots[attackQ[combatPosition.Attacker]].Name, attackQ[combatPosition.Attacker]);
+                    _Hands[combatPosition.Attacker].slots.RemoveAt(combatPosition.AttackQ[combatPosition.Attacker]);
                 }
                 else
                 {
-                    attackQ[currentAttacker] = attackQ[currentAttacker] == _Hands[currentAttacker].slots.Count - 1 ? 0 : attackQ[currentAttacker] + 1;
+                    combatPosition.AttackQ[combatPosition.Attacker] = combatPosition.AttackQ[combatPosition.Attacker] == _Hands[combatPosition.Attacker].slots.Count - 1 ? 0 : combatPosition.AttackQ[combatPosition.Attacker] + 1;
                 }
                 if (_Hands[0].slots.Count == 0 || _Hands[1].slots.Count == 0)
                 {
                     totalBattles++;
-                    if (_Hands[0].slots.Count == 0) 
+                    if (_Hands[0].slots.Count == 0)
                     {
                         if (_Hands[1].slots.Count == 0)
                         {
@@ -165,12 +204,14 @@ namespace BGCompanion
                 }
                 else
                 {
-
-                    Combat(_Hands, attackQ, currentAttacker ^ 1, -1);
+                    combatPosition.Attacker = combatPosition.Attacker ^ 1;
+                    combatPosition.Target = -1;
+                    Combat(_Hands, combatPosition);
                 }
             }
+
         }
-        
-        
+
+
     }
 }
